a6ef82940319d5cce23d7b2a4c4f5a88b013f83e
renzo <renvmorales@gmail.com>
2017-10-27 20:13:17 -0200

    Add scraper.py file with input check.

---
+++ phone-email-scraper/scraper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import pyperclip as pyclip    # install module via pip3 install pyperclip
+import sys
+
+# Instructions: copy all text (ctr-A + ctrl-C) from 
+# http://cdm266101.cdmhost.com/cdm/ref/collection/p266101coll7/id/25785
+
+
+print('\nPhone and email scraper')
+print('Copy all text (ctrl-A + ctrl-C) from:\n')
+print("\thttp://cdm266101.cdmhost.com/cdm/ref/collection/p266101coll7/id/25785")
+
+print('\nWhen done press <enter>.')
+
+dummy = input()
+
+
+if not dummy:
+	print('\nCopying text to the clipboard ...')
+	document = pyclip.paste()
+else:
+	print('You have not hit <Enter> key.')
+	sys.exit()
+
+
+	
