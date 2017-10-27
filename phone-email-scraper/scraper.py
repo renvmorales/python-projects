79ca7acd8311f0b35b8fb39977052f25855db9fa
renzo <renvmorales@gmail.com>
2017-10-27 21:02:04 -0200

    Add a first regular expression for phone number patterns

--- phone-email-scraper/scraper.py
+++ phone-email-scraper/scraper.py
@@ -2,7 +2,7 @@
 
 import pyperclip as pyclip    # install module via pip3 install pyperclip
 import sys
-
+import re
 
 
 # Directions to user: copy all text (ctr-A + ctrl-C) from 
@@ -28,5 +28,20 @@ else:
 
 
 
+# set phone number Regex
+phoneRegex = re.compile(r'''  
+	# phone number patterns to match:
+	## 500-100-0000  (500)-100-0000   500 100 0000
+	(
+	((\d){3}|\(\d{3}\))?	# code area
+	(-|\s)		# first dash separator/whitespace
+	(\d){3}			# 3 digits
+	(-|\s)		# second dash separator/whitespace
+	(\d){4}			# 4 last digits
+	)
+	''', re.VERBOSE)
+
+
+allPhones = phoneRegex.findall(document)
 
 
