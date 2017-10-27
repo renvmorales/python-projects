7eb132d9109f55ea8773d437bbf229fb62acecd4
renzo <renvmorales@gmail.com>
2017-10-27 20:19:30 -0200

    Add comments on user directions and checking the input <Enter> key.

--- phone-email-scraper/scraper.py
+++ phone-email-scraper/scraper.py
@@ -3,19 +3,22 @@
 import pyperclip as pyclip    # install module via pip3 install pyperclip
 import sys
 
-# Instructions: copy all text (ctr-A + ctrl-C) from 
+
+
+# Directions to user: copy all text (ctr-A + ctrl-C) from 
 # http://cdm266101.cdmhost.com/cdm/ref/collection/p266101coll7/id/25785
-
-
 print('\nPhone and email scraper')
 print('Copy all text (ctrl-A + ctrl-C) from:\n')
 print("\thttp://cdm266101.cdmhost.com/cdm/ref/collection/p266101coll7/id/25785")
 
-print('\nWhen done press <enter>.')
 
+print('\nWhen done press <enter>.')
 dummy = input()
 
 
+
+
+# Check if input is the <Enter> key
 if not dummy:
 	print('\nCopying text to the clipboard ...')
 	document = pyclip.paste()
@@ -24,4 +27,6 @@ else:
 	sys.exit()
 
 
-	
+
+
+
