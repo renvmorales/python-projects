fcdff2cd0712ea4bc18224f5a119dd70bd9983ce
renzo <renvmorales@gmail.com>
2017-10-27 22:21:34 -0200

    Add extension numbers to phoneRegex and store phone numbers in a list.

--- phone-email-scraper/scraper.py
+++ phone-email-scraper/scraper.py
@@ -31,17 +31,25 @@ else:
 # set phone number Regex
 phoneRegex = re.compile(r'''  
 	# phone number patterns to match:
-	## 500-100-0000  (500)-100-0000   500 100 0000
+	# 500-100-0000, (500)-100-0000, 100 0000 ext 12345, ext. 12345, x12345
 	(
-	((\d){3}|\(\d{3}\))?	# code area
+	((\d){3}|\((\d){3}\))?	# code area
 	(-|\s)		# first dash separator/whitespace
-	(\d){3}			# 3 digits
+	(\d){3}		# 3 digits
 	(-|\s)		# second dash separator/whitespace
-	(\d){4}			# 4 last digits
+	(\d){4}		# 4 last digits
+	((ext\.|x)(\s)?\d{2,5})?
 	)
 	''', re.VERBOSE)
 
 
+# find all phone numbers according the pattern
 allPhones = phoneRegex.findall(document)
 
 
+# store in a list all phone numbers from query
+phoneNumbers = [phone[0] for phone in allPhones]
+
+
+# print(phoneNumbers)
+print('Total phone numbers found: %d' % len(phoneNumbers))
