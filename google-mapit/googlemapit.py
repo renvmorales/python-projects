2308ecdce9089bd0c6fb57a28581512f4d17e439
renzo <renvmorales@gmail.com>
2017-11-07 11:20:02 -0200

    Include google-mapit folder

---
+++ google-mapit/googlemapit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import webbrowser, sys, pyperclip
+
+
+# adress as optional external argument 
+# >> googlemapit.py 196 Lisbon Av. USA
+# where sys.argv = ['googlemapit.py', '196', 'Lisbon', 'Av.', 'USA']
+
+
+if len(sys.argv)>1:
+	# join all strings together
+	address = ' '.join(sys.argv[1:])
+else:
+	address = pyperclip.paste()
+
+
+# add address to the google maps
+webbrowser.open('https://www.google.com/maps/place/' + address)
+sys.exit()
