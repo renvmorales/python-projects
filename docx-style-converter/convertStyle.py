90e150fe2701bfcc755cf59280cc1a8116309bea
renzo <renvmorales@gmail.com>
2017-11-13 20:04:03 -0200

    Add changes on style of Heading 2 and Heading 3

--- docx-style-converter/convertStyle.py
+++ docx-style-converter/convertStyle.py
@@ -16,11 +16,23 @@ doc = docx.Document('tutorial.docx')
 
 
 
-# define the level of all changes
+# define all changes at every level of the document body
 style = doc.styles['Normal']
 font = style.font		# get a font attribute
 font.size = Pt(8)		# specify font size
 font.name = 'Arial'		# specify font style
+
+style2 = doc.styles['Heading 2']
+font2 = style2.font		# get a font attribute
+font2.size = Pt(16)		# specify font size
+font2.bold = True
+font2.name = 'Arial'	# specify font style
+
+style3 = doc.styles['Heading 3']
+font3 = style3.font		# get a font attribute
+font3.size = Pt(12)		# specify font size
+font3.name = 'Cambria'	# specify font style
+
 
 
 
