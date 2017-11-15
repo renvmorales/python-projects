be73c1cb33447695b6d4ac68879e8d3ca66d418e
renzo <renvmorales@gmail.com>
2017-11-15 18:39:43 -0200

    Add a first version of a web scraper of Capes Qualis

---
+++ web-scraper/CapesQualisScraper.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+# import requests, sys
+from selenium import webdriver
+from time import sleep
+import bs4
+
+
+
+# url address 
+url = 'https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/veiculoPublicacaoQualis/listaConsultaGeralPeriodicos.xhtml'
+
+
+
+
+# try:	
+# 	res = requests.get(url) # get data from address
+# 	res.raise_for_status() # test if connection is successful
+# except (requests.exceptions.HTTPError, MissingSchema):
+# 	print('\nYou have entered an incorrect address.')
+# 	sys.exit()
+
+
+
+
+# launch browser
+browser = webdriver.Chrome()
+
+
+
+# open the url on the browser
+browser.get(url)
+
+
+
+
+# select option: CLASSIFICAÇÕES DE PERIÓDICOS QUADRIÊNIO 2013-2016
+browser.find_element_by_xpath('//*[@id="form:evento"]/option[2]').click()
+
+
+
+# select option: MATEMÁTICA PROBABILIDADE E ESTATÍSTICA
+browser.find_element_by_xpath('//*[@id="form:area"]/option[36]').click()
+
+# add this option
+browser.find_element_by_xpath('//*[@id="form:adicionarArea"]/span').click()
+
+
+
+
+# select option: A1
+browser.find_element_by_xpath('//*[@id="form:estrato"]/option[2]').click()
+
+
+
+sleep(2)
