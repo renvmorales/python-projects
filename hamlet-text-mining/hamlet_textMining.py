924a8973dbceca5be0f34a72d56b9d8ce346a00c
renzo <renvmorales@gmail.com>
2017-11-19 21:34:57 -0200

    Convert abbreviation to names, and get word counts.

--- hamlet-text-mining/hamlet_textMining.py
+++ hamlet-text-mining/hamlet_textMining.py
@@ -5,6 +5,9 @@ from string import punctuation
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from nltk.stem import PorterStemmer
+import pandas as pd 
+from util import abbrev2names, wordCount
+
 
 
 # url address with an online version of Shakespeare's Hamlet
@@ -35,43 +38,53 @@ nopunc = [char for char in hamlet if char not in punctuation]
 nopunc = ''.join(nopunc)
 
 
-
-# word stemming object
-ps = PorterStemmer()
-
-# stem words and get all text back 
-text = []
-for w in nopunc.split():
-	text.append(ps.stem(w.lower()))
+# convert abbreviations to names
+nopunc = abbrev2names(nopunc.split())
 
 
 
 
+# # word stemming object
+# ps = PorterStemmer()
 
-# word counter function
-def word_count(text_ls):
-# initialize dictionary with counts
-	count = {}
+# # stem words and get all text back 
+# text = []
+# for w in nopunc.split():
+# 	text.append(ps.stem(w.lower()))
 
-# word counter loop
-	for w in text_ls:
-		count.setdefault(w, 0)
-		count[w]+=1
 
-	return count
+
 
 
 # get the word counts from the text
-word_count = word_count(text)
+word_count = wordCount(nopunc)
 
 
+
+# transform dicitonary to dataframe 
+df = pd.DataFrame(data=list(word_count.values()), 
+	index=word_count.keys(), columns=['Word_Count'])
+
+
+# sort counts in descending order 
+df = df.sort_values(by=['Word_Count'],ascending=False) 
+
+
+print('\nThe most frequent used words:')
+print(df.head(20))
+
+
+
+
+# join all words into a long string
+text = ' '.join(nopunc)
 
 
 
 # create a custom word cloud
 wordcloud = WordCloud(max_words=50, width=800, height=400, 
-	collocations=False, #stopwords=[],
-	background_color='white').generate(nopunc)
+	collocations=False, stopwords=[],
+	background_color='white').generate(text)
 
 
 
@@ -82,3 +95,23 @@ print('\nCreating a word cloud of all words ...')
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
+
+
+
+
+
+# create a second word cloud
+wordcloud2 = WordCloud(max_words=50, width=800, height=400, 
+	collocations=False, 
+	background_color='white').generate(text)
+
+
+
+
+print('\nCreating a word cloud discarding stopwords ...')
+
+# display word cloud
+plt.imshow(wordcloud2, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
